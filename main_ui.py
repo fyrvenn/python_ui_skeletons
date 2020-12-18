8b48b5688b9847880089428c7807b7d9fc768e4e
Ekaterina Budnikova <e.budnikova@repo.ddg>
2020-12-18 13:50:14 +0300

    Just okoshechko =)

---
+++ main_ui.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from tkinter import *
+
+
+def launch_ui():
+    root = Tk()
+    root.resizable(width=False, height=False)
+    root.mainloop()
---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import main_ui
+
+
+def main():
+    main_ui.launch_ui()
+
+if __name__ == "__main__":
+    main()
