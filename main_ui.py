b2f8ec440ab5988fe35bb010ce4ed94deba66d7d
Ekaterina Budnikova <e.budnikova@repo.ddg>
2020-12-18 17:14:21 +0300

    Combobox and one more frame.

--- main_ui.py
+++ main_ui.py
@@ -2,8 +2,8 @@
 
 from tkinter import *
 import tkinter.messagebox as mbox
+from tkinter import ttk
 from tkinter import filedialog as fd
-
 
 font_std = "Helvetica 12"
 
@@ -17,21 +17,63 @@ def return_error():
     sys.exit(1)
 
 
-def launch_ui():
-    root = Tk()
-    root.title('Python UI unit')
-    root.resizable(width=False, height=False)
+class MainClass(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
 
-    frame_header = Frame(root, bg='green', bd=5)
-    frame_chb = Frame(root, bg='red', bd=5)
-    frame_result = Frame(root, bg='light gray', bd=5)
-    frame_logger = Frame(root, bg='gray', bd=5)
-    frame_footer = Frame(root, bg='white', bd=5)
+        self.master.title('Python UI unit')
+        self.master.resizable(width=False, height=False)
 
-    text_win = Text(frame_logger, font='Courier', width=30, height=15, bg='white')
-    text_win.pack(anchor=W, expand=1, fill=BOTH)
+        # Objects
+        self.frame_header = Frame(self.master, bg='green', bd=5)
+        self.frame_chb = Frame(self.master, bg='red', bd=5)
+        self.frame_rb = Frame(self.master, bg='light gray', bd=5)
+        self.frame_cb = Frame(self.master, bg='light blue', bd=5)
+        self.frame_win = Frame(self.master, bg='gray', bd=5)
+        self.frame_footer = Frame(self.master, bg='white', bd=5)
 
-    def open_file():
+        self.text_win = Text(self.frame_win, font='Courier', width=30, height=15, bg='white')
+
+        self.cbb = ttk.Combobox(self.frame_cb, width=3, values='')
+
+        self.l_header = Label(self.frame_header, width=80, height=5, font=font_std + " bold", text="HEADER",
+                              bg="dark green")
+
+        self.l_chb = Label(self.frame_chb, width=25, height=30, text="CHECKBOX", bg="red")
+        self.l_rb = Label(self.frame_rb, width=25, height=30, text="RADIOBUTTON", bg="light gray")
+        self.l_l = Label(self.frame_cb, width=25, text="Выбрать:", bg="light blue")
+
+        self.main_btn = Button(self.frame_footer, font=font_std, bg="light gray", width=20, height=2, text=u"Открыть файл",
+                               command=self.open_file)
+        self.error_btn = Button(self.frame_footer, font=font_std + " bold", bg="light gray", fg="red", width=20,
+                                height=2,
+                                text=u"Не нажимать!!!", command=return_error)
+        self.quit_btn = Button(self.frame_footer, font=font_std, bg="light gray", width=20, height=2, text=u"Выход",
+                               command=quit_prog)
+
+        # Pack
+        self.frame_header.pack(side=TOP, fill=X, anchor=N)
+        self.frame_footer.pack(side=BOTTOM, fill=X, anchor=S)
+        self.frame_chb.pack(side=LEFT, fill=Y, anchor=NW)
+        self.frame_win.pack(side=RIGHT, expand=1, fill=BOTH, anchor=NE)
+        self.frame_cb.pack(side=TOP, fill=BOTH, anchor=S)
+        self.frame_rb.pack(side=BOTTOM, fill=Y, anchor=N)
+
+        self.main_btn.pack(anchor=SW, side='left')
+        self.quit_btn.pack(anchor=SE, side='right')
+        self.error_btn.pack(anchor='center', side='bottom')
+
+        self.text_win.pack(anchor=W, expand=1, fill=BOTH)
+        self.l_l.pack()
+        self.cbb.pack(side='top', fill=BOTH)
+
+        self.l_header.pack()
+        self.l_chb.pack()
+        self.l_rb.pack()
+
+    def open_file(self):
         file_path = fd.askopenfilename(defaultextension='.txt',
                                        filetypes=[('File Text', '*.txt'),
                                                   ('All files', '*.*')])
@@ -39,32 +81,12 @@ def launch_ui():
             filename = open(file_path, "r", encoding='utf-8')
             print(file_path)
             content = filename.read()
-            text_win.insert(END, content)
+            self.text_win.insert(END, content)
         except Exception:
-            print("File not found")
+            pass
 
-    l_header = Label(frame_header, width=80, height=5, text="HEADER", bg="green")
-    l_chb = Label(frame_chb, width=25, height=30, text="CHECKBOX", bg="red")
-    l_rb = Label(frame_result, width=25, height=30, text="RADIOBUTTON", bg="light gray")
 
-    main_btn = Button(frame_footer, font=font_std, bg="light gray", width=20, height=2, text=u"Кнопка",
-                      command=open_file)
-    error_btn = Button(frame_footer, font=font_std+" bold italic", bg="light gray", fg="red", width=20, height=2,
-                       text=u"Не нажимать!!!", command=return_error)
-    quit_btn = Button(frame_footer, font=font_std, bg="light gray", width=20, height=2, text=u"Выход",
-                      command=quit_prog)
-
-    frame_header.pack(side=TOP, fill=X, anchor=N)
-    frame_footer.pack(side=BOTTOM, fill=X, anchor=S)
-    frame_chb.pack(side=LEFT, fill=Y, anchor=NW)
-    frame_result.pack(side=LEFT, fill=Y, anchor=NW)
-    frame_logger.pack(side=RIGHT, expand=1, fill=BOTH, anchor=NE)
-    main_btn.pack(anchor=SW, side='left')
-    quit_btn.pack(anchor=SE, side='right')
-    error_btn.pack(anchor='center', side='bottom')
-
-    l_header.pack()
-    l_chb.pack()
-    l_rb.pack()
-
-    root.mainloop()
+def launch_ui():
+    root = Tk()
+    ui = MainClass(master=root)
+    ui.mainloop()
